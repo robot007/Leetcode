ed9af97ac000740cdafedb3f486521a4f3c04cf7
Zhen Song <zhen.song@siemens.com>
2016-10-03 16:18:51 -0400

    unit test.

--- VSUnitTest/UnitTest/unittest1.cpp
+++ VSUnitTest/UnitTest/unittest1.cpp
@@ -8,11 +8,25 @@ namespace UnitTest
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		// int test();
+
+		TEST_METHOD(Test1)
 		{
 			// TODO: Your test code here
+			Assert::AreEqual(1, 1);
+			
 		}
 
+		TEST_METHOD(Test2)
+		{
+			Assert(test() );
+		}
+
+		int test(void )
+		{
+			return 1;
+		}
 	};
+
+
 }
