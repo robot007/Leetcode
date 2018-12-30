a191605cb8db766bd0ebfb75a41ad74bdcb626b9
Zhen Song <zhen.song@siemens.com>
2018-12-30 10:12:31 -0600

    java

--- Python/remove_duplicates.py
+++ Python/remove_duplicates.py
@@ -1,4 +1,5 @@
 '''
+
 Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 Do not allocate extra space for another array, you must do this in place with constant memory.
 For example, Given input array A = [1,1,2],
