8a33a30e4998bc88463638179e50f49dcd667744
Zhen Song <zhensong23931@gmail.com>
2019-01-15 07:07:25 -0600

    java

--- Python/QuickSort.py
+++ Python/QuickSort.py
@@ -7,8 +7,7 @@ def qsort(A, lo, hi):
         print(f'A={A}, lo={lo}, hi={hi}, p={p}')
         qsort(A, lo, p)
         qsort(A, p+1, hi)
-    else:
-        print('leaf end')    
+  
 
 def swap(a, b):
     a=a+b
@@ -18,21 +17,33 @@ def swap(a, b):
 
 def partition(A, lo, hi):
     lo = int(lo)
+    i = lo
     hi = int(hi)
+    j = hi
     p = int( (lo+hi)/2 )
+    bypass=False
     while lo < hi:
-        print(f'p={p}, lo={lo}, hi={hi}, A[p]={A[p]}, A[lo]={A[lo]}')        
-        while A[lo] <= A[p]:
+        print(f'A={A}, lo={lo}, p={p}, hi={hi}, A[lo]={A[lo]}, A[p]={A[p]}, A[hi]={A[hi]} ')        
+        while A[lo] <= A[p] and lo<p:
             lo+=1
-        while A[hi] >= A[p]:
+            bypass = True
+            # break
+        while A[hi] >= A[p] and hi>p:
             hi-=1
-        if lo>hi:
-            p=hi
-            return p
-        else:
-            print(f'B: swap A[{lo}] with A=[{hi}]')
-            (A[lo], A[hi]) = swap(A[lo], A[hi])
-            print(f'A: A={A}')
+            bypass = True
+            # break
+        if bypass==False:
+            if i>j:
+                return j
+            else:
+                print(f'B: swap A[{i}] with A[{j}]')
+                a,  b = swap(A[i], A[j])
+                A[i]=a
+                A[j]=b
+                print(f'A: A={A}')
+        i = lo
+        j = hi
+        bypass=False
         # if A[hi] >= A[p]:
         #     hi -= 1
         # elif A[lo] <= A[p]:
@@ -51,7 +62,7 @@ def partition(A, lo, hi):
 
 def test():
     A=[5, 4, 8, 3, 2, 1]
-    print('beofre: A='+str(A))
+    print('before: A='+str(A))
     qsort(A, 0, len(A)-1)
     # print('after: A='+ str(A))
 
