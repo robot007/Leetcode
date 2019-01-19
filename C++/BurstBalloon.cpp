23bf8ccdb0eb6fa7bd589cc720cab2a7111c8be7
Zhen Song <zhensong23931@gmail.com>
2019-01-18 23:20:34 -0600

    on going

--- C++/BurstBalloon.cpp
+++ C++/BurstBalloon.cpp
@@ -1,4 +1,4 @@
-
+// 312
 // Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
 
 // Find the maximum coins you can collect by bursting the balloons wisely.
@@ -8,8 +8,7 @@
 // You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
 // 0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
 
-// # Wrong Answer 1/18/19
-// [9,76,64,21,97,60,5], expected 1088290, output 1088286
+
 
 class Solution {
 public:
