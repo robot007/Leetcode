160c64e7ea0fb4a1b217a8132d113279fd7056bc
Zhen Song <zhensong23931@gmail.com>
2019-01-19 10:05:00 -0600

    two sum full Java

---
+++ Java/TwoSum.java
@@ -0,0 +1,26 @@
+public class Solution
+{
+    
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    
+    public static void main(String[] arg)
+    {
+        int[] nums = {2,7,11,5};
+        Solution sol = new Solution();
+        int[] ret = sol.twoSum(nums, 12);
+        
+        System.out.println(ret[0]);
+        System.out.println(ret[1]);
+            
+    }
+}
